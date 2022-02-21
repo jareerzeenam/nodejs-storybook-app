618aa701ccf51337d5a78c0dd356d6be48f01a7c
jareer <jareerzeenam@adventus.io>
2022-02-21 11:48:07 +0530

    Handle bars added

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//morgan login
+const morgan = require('morgan');
+// express handle bar template engine
+const exphbs = require('express-handlebars');
 
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
 
+// Connect to db
 connectDB();
 
 const app = express();
+
+// Handlebars
+app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 3000;
 
--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // MONGO_URI is in ./config/config.env
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
