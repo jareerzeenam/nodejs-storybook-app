f85e79ef34f4e94f7d1cf78573f74c1326df591e
jareer <jareerzeenam@adventus.io>
2022-02-22 00:15:33 +0530

    Authenticate with Google and store user in mongoDB done

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ const morgan = require('morgan');
 // express handle bar template engine
 const { engine } = require('express-handlebars');
 
+//Passport
+const passport = require('passport');
+//Express Session
+const session = require('express-session');
+
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+//Passport Config (Authenticate user)
+require('./config/passport')(passport);
 
 // Connect to mongo db
 connectDB();
@@ -26,11 +34,25 @@ app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+//Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //! Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 //! Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 
