71c03325c72177c5302462d8502a5303d5786e30
jareer <jareerzeenam@adventus.io>
2022-02-21 22:29:19 +0530

    Static folder and file added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 //morgan login
@@ -15,14 +16,18 @@ connectDB();
 
 const app = express();
 
+//Logging
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
 //Handlebars (template engine)
 app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
-if (process.env.NODE_ENV == 'development') {
-  app.use(morgan('dev'));
-}
+//! Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //! Routes
 app.use('/', require('./routes/index'));
